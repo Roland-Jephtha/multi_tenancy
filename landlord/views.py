e4a415bc7ab082073c51ab9380d479e755c25272
unknown <jephtroland832@gmail.com>
2025-06-08 23:46:27 +0100

    template

--- landlord/views.py
+++ landlord/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+
+
+def login(request):
+    return render(request, 'auth/login.html')
+
+def signup(request):
+    return render(request, 'auth/register.html')

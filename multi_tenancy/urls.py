a5c1eff0efdd1c8efadfc847edbf8b5f6265a714
Jonathan Ikpen <jonathanikpen4@gmail.com>
2025-06-09 23:34:06 +0100

    feat: add index page

--- multi_tenancy/urls.py
+++ multi_tenancy/urls.py
@@ -2,19 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from multi_tenancy.views import login, signup
+from multi_tenancy.views import login, signup, index
 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
     path('signup/', signup, name='signup'),
     path('login/', login, name='login'),
     path('landlord/', include('landlord.urls'))
 ]
-
-
 
 
 

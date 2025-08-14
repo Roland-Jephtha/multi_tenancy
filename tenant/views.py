e38d9c4eb80fbd6002925a01317c471838ccb1f3
Jonathan Ikpen <jonathanikpen4@gmail.com>
2025-08-14 17:42:36 +0100

    feat: added tenant dashboard

--- tenant/views.py
+++ tenant/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+from .models import Tenant
 
 # Create your views here.
+@login_required
+def dashboard(request):
+    # if request.user.position == 'tenant':
+    #     profile = Tenant.objects.get(user = request.user)
+    
+    # context = {
+    #     'profile':profile
+    # }
+    
+    return render(request, 'dashboard/tenant_dashboard.html')
